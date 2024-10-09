e0409cc50afe56fd81ead907cf9be1d1801a2c63
thanhcong200 <vuthanhcong110502@gmail.com>
2024-10-09 10:53:05 +0700

    fix: throw error generate api

--- src/api/ai-app/controllers/ai-app.js
+++ src/api/ai-app/controllers/ai-app.js
@@ -11,6 +11,8 @@ module.exports = createCoreController('api::ai-app.ai-app', ({ strapi }) => ({
         const result = await strapi
             .service("api::ai-app.ai-app")
             .generate(ctx);
+        if(result.status === 400) 
+            return ctx.badRequest(JSON.stringify(result.data));
         ctx.send(result);
     },
     async like(ctx) {
