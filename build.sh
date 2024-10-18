ee4a735046bc21cc423af8e5df295f67f3da451a
dainv7 <dainv7@vpbank.com.vn>
2024-10-18 15:18:38 +0700

    uncommout push command

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 docker build -t eda-webportal-be .
 docker tag eda-webportal-be:latest 891377012613.dkr.ecr.ap-southeast-1.amazonaws.com/eda-webportal:be-20240926
-# docker push 891377012613.dkr.ecr.ap-southeast-1.amazonaws.com/eda-webportal:be-20240926
+docker push 891377012613.dkr.ecr.ap-southeast-1.amazonaws.com/eda-webportal:be-20240926
 docker rmi $(docker images --filter "dangling=true" -q --no-trunc)
